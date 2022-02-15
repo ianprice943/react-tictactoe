0447ca038499babb19bb057d1a3fb5447715b678
Ian Price <ianprice943@yahoo.com>
2022-02-15 14:28:16 -0500

    added 9 step rewind test

--- tests/integration/GameBoard.test.tsx
+++ tests/integration/GameBoard.test.tsx
@@ -332,4 +332,28 @@ describe("The GameBoard component state", () => {
         expect(board.find(Square).at(6).props().value).toBe("");
         expect(board.find(Square).at(8).props().value).toBe("");
     });
+
+    it("should rewind the game nine steps", () => {
+        board.find(Square).at(0).props().clickHandler();
+        board.find(Square).at(4).props().clickHandler();
+        board.find(Square).at(1).props().clickHandler();
+        board.find(Square).at(3).props().clickHandler();
+        board.find(Square).at(5).props().clickHandler();
+        board.find(Square).at(2).props().clickHandler();
+        board.find(Square).at(6).props().clickHandler();
+        board.find(Square).at(8).props().clickHandler();
+        board.find(Square).at(7).props().clickHandler();
+        for(let i = 0; i < 9; i++) {
+            board.find(RewindGameButton).props().rewindClickHandler();
+        }
+        expect(board.find(Square).at(4).props().value).toBe("");
+        expect(board.find(Square).at(0).props().value).toBe("");
+        expect(board.find(Square).at(1).props().value).toBe("");
+        expect(board.find(Square).at(3).props().value).toBe("");
+        expect(board.find(Square).at(5).props().value).toBe("");
+        expect(board.find(Square).at(2).props().value).toBe("");
+        expect(board.find(Square).at(6).props().value).toBe("");
+        expect(board.find(Square).at(8).props().value).toBe("");
+        expect(board.find(Square).at(7).props().value).toBe("");
+    });
 });
