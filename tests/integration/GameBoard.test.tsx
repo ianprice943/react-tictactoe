ef908944c5a9d31d0ddd9fbe5895137ac199b2dc
Ian Price <ianprice943@yahoo.com>
2022-02-15 14:17:19 -0500

    updated two step test to test both squares

--- tests/integration/GameBoard.test.tsx
+++ tests/integration/GameBoard.test.tsx
@@ -220,4 +220,14 @@ describe("The GameBoard component state", () => {
         }
         expect(board.find(Square).at(0).props().value).toBe("");
     });
+
+    it("should rewind the game two steps", () => {
+        board.find(Square).at(0).props().clickHandler();
+        board.find(Square).at(4).props().clickHandler();
+        for(let i = 0; i < 2; i++) {
+            board.find(RewindGameButton).props().rewindClickHandler();
+        }
+        expect(board.find(Square).at(4).props().value).toBe("");
+        expect(board.find(Square).at(0).props().value).toBe("");
+    });
 });
